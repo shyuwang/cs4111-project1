b4492128e4cb3e1b0646c2d72cdde28d34e17f35
shyuwang <sw3449@columbia.edu>
2020-11-05 17:19:58 -0500

    add username and password

--- server.py
+++ server.py
@@ -29,7 +29,7 @@ app = Flask(__name__, template_folder=tmpl_dir)
 #
 #     DATABASEURI = "postgresql://gravano:foobar@34.75.150.200/proj1part2"
 #
-DATABASEURI = "postgresql://user:password@34.75.150.200/proj1part2"
+DATABASEURI = "postgresql://sw3449:6294@34.75.150.200/proj1part2"
 
 
 #
