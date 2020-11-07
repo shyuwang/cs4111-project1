25c1755e70ab1c91045459566a59bbca724c0331
shyuwang <sw3449@columbia.edu>
2020-11-07 18:33:23 -0500

    Add form fields

---
+++ wtform_fields.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired
+
+class RegistrationForm(FlaskForm):
+    """ Registration form """
+    username = StringField('username_label', validators=[InputRequired(message="Username required")])
+    password = PasswordField('password_label', validators=[InputRequired(message="Password required")])
+    submit_button = SubmitField('Create')
+
+class LoginForm(FlaskForm):
+    """ Login Form """
+    username = StringField('username_label', validators=[InputRequired(message="Username required")])
+    password = PasswordField('password_label',validators=[InputRequired(message="Password required")])
+    submit_button = SubmitField('Login')
+
+
